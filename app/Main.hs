59cadb42a4275ea91a2eaf1af65741552af91dfd
groscoe <gustavo.roscoe@waycarbon.com>
2018-06-03 19:18:48 -0300

    Cleaned up Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,6 @@ module Main where
 
 import Repl (runRepl)
 
--- main :: IO ()
--- main = do
---   args <- getArgs
---   case args of
---     [fileName] -> do
---       source <- readFile fileName
---       case run source of
---         Right result -> print result
---         Left  err    -> putStrLn $ "Error: " ++ err
---     badArgs   -> putStrLn $ "Wrong number of arguments: " ++ show (length badArgs)
-
 main :: IO ()
 main = do
   putStrLn "Welcome to the LET language!"
